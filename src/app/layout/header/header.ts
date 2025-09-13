cdf4f69c1d1bf034144bf1f378d4d5d66b15aaa2
vigneshs-ccl <vigneshkumar.s@crystalcodelabs.com>
2025-09-13 18:45:11 +0530

    feat:completed header

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,11 +1,31 @@
+
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgSelectModule } from '@ng-select/ng-select';
+
+interface Languages  {
+  lang:string;
+  val:string;
+}
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone:true,
+  imports: [FormsModule,CommonModule,NgSelectModule],
   templateUrl: './header.html',
-  styleUrl: './header.scss'
+  styleUrls: ['./header.scss']
 })
 export class Header {
+  languages:Languages[] = [
+    { lang: 'English', val: 'en' },
+    { lang: 'German', val: 'de' },
+    { lang: 'French', val: 'fr' },
+    { lang: 'Hindi', val: 'hi' },
+  ];
+
+  // selectedLanguage: Languages = this.languages[0];
+
+  selectedLanguage = "en"
 
 }
